8a2587e379684c7986b1753ad268f018e634a9f5
Sonya <sonyagudilova254@gmail.com>
2024-11-21 17:28:21 +0300

    Реаллизация класса Main для вывода результатов.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,1 +1,23 @@
 package org.example;
+
+import java.util.List;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        PersonReader personReader = new PersonReader();
+        List<Person> people = personReader.readCsvFile("foreign_names.csv", ";");
+
+        for (Person person : people)
+        {
+            System.out.println("Person ID: " + person.getId());
+            System.out.println("Name: " + person.getName());
+            System.out.println("Gender: " + person.getGender());
+            System.out.println("Birth Date: " + person.getDateOfBirth());
+            System.out.println("Department: " + person.getDepartament().getId()+ person.getDepartament().getName());
+            System.out.println("Salary: " + person.getSalary());
+            System.out.println("------------------------------");
+        }
+    }
+}
