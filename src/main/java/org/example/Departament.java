cf4a15c1aeb2f5f2d25d579381181bd00759a239
Sonya <sonyagudilova254@gmail.com>
2024-11-21 16:02:21 +0300

    Реализация класса департамент. Руализация конструктора и геттеров для обьектов класса.

---
+++ src/main/java/org/example/Departament.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Departament{
+    private int id;
+    private String name;
+
+    public Departament(int id, String name){
+        this.id=id;
+        this.name=name;
+    }
+    //get
+    public int getId()
+    {
+        return id;
+    }
+    public String getName()
+    {
+        return name;
+    }
+}
+
+
