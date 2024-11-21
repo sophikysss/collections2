ea957f2ab490160640dae361b434074a2fb1ca20
Sonya <sonyagudilova254@gmail.com>
2024-11-21 17:13:55 +0300

    .

--- src/main/java/org/example/PersonReader.java
+++ src/main/java/org/example/PersonReader.java
@@ -4,10 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
